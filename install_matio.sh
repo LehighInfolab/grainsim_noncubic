f0496842c30f8b5f28b61d0d7cdf10e5a4e0aca6
suru_grainsim_sol <suh222@lehigh.edu>
2026-07-12 20:52:56 -0400

    update serial hdf5 for hpc.

--- install_matio.sh
+++ install_matio.sh
@@ -1,16 +1,18 @@
-module load gcc/12.4.0 cmake/3.30.5 hdf5
+module load gcc/12.4.0 cmake/3.30.5 # hdf5
 cmake --version
 
 wget https://github.com/tbeu/matio/releases/download/v1.5.28/matio-1.5.28.tar.gz
 tar xzf matio-1.5.28.tar.gz
 cd matio-1.5.28
-mkdir build && cd build
+rm -rf build && mkdir build && cd build
 
 cmake .. \
   -DCMAKE_INSTALL_PREFIX=$HOME/local \
   -DMATIO_MAT73=ON \
   -DMATIO_SHARED=ON \
-  -DBUILD_TESTING=OFF
+  -DBUILD_TESTING=OFF \
+  -DHDF5_ROOT=$HOME/local \
+  -DHDF5_PREFER_PARALLEL=OFF
 make -j4
 make install
 echo $?
